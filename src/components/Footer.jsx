9befece81b7e990825c6b5ad17203a56135a7fd9
Pankaj <pankaj.kumar.phe21@itbhu.ac.in>
2024-06-03 10:05:33 +0530

    footer responsified

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,9 @@ import Image from 'next/image'
 
 const Footer = () => {
   return (
-    <div className='flex flex-col mt-[180px] w-[100%] max-sm:mx-[5%] mx-[7%] items-center'>
+    <div className='flex flex-col mt-[180px] w-[100%] max-sm:ml-[5%] mx-[7%] items-center'>
       <div className="flex">
-        <div className="flex flex-col">
+        <div className="flex flex-col max-sm:ml-[30px] max-sm:w-[180px]">
           <div className="flex uifry items-center">
             <Image src='/images/Frame.png' height={34} width={34}/>
             <p className='font-bold text-lg ml-[2px]'>uifry</p>
@@ -20,39 +20,43 @@ const Footer = () => {
             <p className='font-[500] text-sm ml-[2px]'>+1234 456 678 89 </p>
           </div>
         </div>
-        <div className="flex flex-col links mt-[2px] ml-[40px] max-sm:ml-[9vw]">
+        <div className="flex flex-col max-sm:hidden mt-[2px] ml-[40px] max-sm:ml-[9vw]">
           <h1 className='text-xl font-semibold'>Links</h1>
           <a className='font-medium mt-[10px]' href="">Home</a>
           <a className='font-medium mt-[10px]'  href="">About Us</a>
           <a className='font-medium mt-[10px]'  href="">Bookings</a>
           <a className='font-medium mt-[10px]'  href="">Blog</a>
         </div>
-        <div className="flex flex-col mt-[2px] ml-[40px] max-sm:ml-[9vw]">
+        <div className="flex flex-col max-sm:hidden mt-[2px] ml-[40px] max-sm:ml-[9vw]">
           <h1 className='text-xl font-semibold'>Legal</h1>
           <a className='font-medium mt-[10px]' href="">Terms Of Use</a>
           <a className='font-medium mt-[10px]'  href="">Privacy Policy</a>
           <a className='font-medium mt-[10px]'  href="">Cookie Policy</a>
         </div>
-        <div className="flex flex-col mt-[2px] ml-[40px] max-sm:ml-[9vw]">
+        <div className="flex flex-col max-sm:hidden mt-[2px] ml-[40px] max-sm:ml-[9vw]">
           <h1 className='text-xl font-semibold'>Products</h1>
           <a className='font-medium mt-[10px]' href="">Take tour</a>
           <a className='font-medium mt-[10px]'  href="">Live Chat</a>
           <a className='font-medium mt-[10px]'  href="">Reviews</a>
         </div>
-        <div className="flex flex-col mt-[2px] ml-[40px] max-sm:ml-[9vw]">
+        <div className="flex flex-col mt-[2px] ml-[40px] max-sm:mr-[20px] max-sm:w-[200px] max-sm:ml-[10px]">
           <h1 className='text-xl font-semibold'>Newsletter</h1>
           <a className='font-medium mt-[10px]' href="">Stay Up to Date</a>
           <div className='mt-[10px] bg-white'>
-          <form action="" method='post'>
+          <form action="" method='post' className='max-sm:hidden'>
           <input className='py-[18px] pl-[20px] outline-none ' placeholder='Your email' type="email"  />
           <input className='bg-black text-white py-3 px-6' type="submit" value="Subscribe"  />
           </form>
           </div>
+          <form action="" method='post' className='sm:hidden'>
+          <input className='py-[18px] pl-[20px]  outline-none ' placeholder='Your email' type="email"  />
+          <input className='mt-[20px] bg-black text-white py-3 px-6' type="submit" value="Subscribe"  />
+          </form>
         </div>
       </div>
       <div className='h-[2px] mb-[20px] mt-[60px] mx-[5%] w-[87%] bg-gradient-to-r from-zinc-100 via-zinc-500 to-zinc-100'></div>
       <div className='flex justify-center mt-[20px] mb-[120px]'>
-      <h1>Copyright 2022 Uifry.Com All Rights Reserved</h1>
+      <h1 className='max-sm:ml-[-30px]'>Copyright 2022 Uifry.Com All Rights Reserved</h1>
       </div>
     </div>
   )
--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -3,16 +3,16 @@ import Image from 'next/image'
 
 const Start = () => {
   return (
-    <div className='bg-zinc-900 text-white h-[500px] mx-[150px] mt-[100px] flex  rounded-md'>
+    <div className='bg-zinc-900 max-sm:w-[370px] max-sm:h-[750px] max-sm:mx-[10px] max-sm:flex-col text-white h-[500px] mx-[150px] mt-[100px] flex  rounded-md'>
         <div className="flex flex-col justify-center gap-3">
-        <h1 className='ml-[100px] font-bold text-4xl'>Ready to Get Started?</h1>
-        <p className='w-[84%] ml-[100px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam suscipit saepe ut.</p>
-        <button className='mt-1 text-black gap-2 ml-[100px]  rounded-md bg-white w-[200px] flex py-[10px] justify-center items-center'>Download App 
-            <Image src='/images/Vector.png' height={31} width={25} />
+        <h1 className='ml-[100px] max-sm:ml-[70px] font-bold text-4xl max-sm:mt-[100px]'>Ready to Get Started?</h1>
+        <p className='w-[84%] ml-[100px] max-sm:ml-[30px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam suscipit saepe ut.</p>
+        <button className='mt-1 max-sm:ml-[70px] text-black gap-2 ml-[100px]  rounded-md bg-white w-[200px] flex py-[10px] justify-center items-center'>Download App 
+            <Image className='' src='/images/Vector.png' height={31} width={25} />
         </button>
         </div>
         <div>
-            <Image className='mt-8' src='/images/iPhone 13 Pro.png' height={459} width={591} />
+            <Image className='mt-8 max-sm:h-[300px] max-sm:w-[370px]' src='/images/iPhone 13 Pro.png' height={459} width={591} />
         </div>
     </div>
   )
